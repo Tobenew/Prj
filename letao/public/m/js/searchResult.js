15cede261ec0901af5d8f762f0ab204dca4f89f5
yinhui <972043362@qq.com>
2019-04-03 13:27:14 +0800

    新建搜索页面

---
+++ letao/public/m/js/searchResult.js
@@ -0,0 +1,22 @@
+window.onload=function () {
+    $(function(){
+        //ssearch搜索关键词
+        var kw = location.search.split("=")[1];
+        var page = 1;
+        var pageSize =2;
+        $.ajax({
+            type: "get",
+            url: "/product/queryProduct",
+            data: {
+                "proName":kw,
+                "page":page,
+                "pageAize":pageSize
+            },
+            success: function (res) {   
+                var html = template("searchResultTpl",res);
+                $("#searchResultList").html(html);
+                
+            }
+        });
+    })
+}
